ff2aeb8020ae1302a65fa654ee055f93c2c73c34
Paolo Pialorsi <paolo@pialorsi.com>
2015-08-20 16:25:08 +0200

    Added SupportedUILanguage to the Domain Model

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Model/SupportedUILanguage.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Model
+{
+    public class SupportedUILanguage : IEquatable<SupportedUILanguage>
+    {
+        public Int32 LCID { get; set; }
+
+        #region Comparison code
+
+        public override int GetHashCode()
+        {
+            return (String.Format("{0}",
+                this.LCID
+                ).GetHashCode());
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is SupportedUILanguage))
+            {
+                return (false);
+            }
+            return (Equals((SupportedUILanguage)obj));
+        }
+
+        public bool Equals(SupportedUILanguage other)
+        {
+            return (this.LCID == other.LCID);
+        }
+
+        #endregion
+    }
+}
