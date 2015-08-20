f5ef2557d40c382af38c48589fb7b84e05f3a428
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-08-20 11:49:14 +0200

    Updated tests and added log config sample

--- Core/OfficeDevPnP.Core.Tests/Diagnostics/PnPMonitoredScopeTests.cs
+++ Core/OfficeDevPnP.Core.Tests/Diagnostics/PnPMonitoredScopeTests.cs
@@ -8,7 +8,7 @@ namespace OfficeDevPnP.Core.Tests.Diagnostics
     public class PnPMonitoredScopeTests
     {
         [TestMethod]
-        public void TextException()
+        public void PnPMonitoredScopeNestingTest()
         {
             using (var ctx = TestCommon.CreateClientContext())
             {
@@ -18,13 +18,19 @@ namespace OfficeDevPnP.Core.Tests.Diagnostics
                     Assert.IsTrue(scope1.CorrelationId != Guid.Empty);
                     Assert.IsTrue(scope1.Parent == null);
 
+                    scope1.LogInfo("Logging from scope 1");
+
                     using (var scope1_1 = new PnPMonitoredScope("1.1"))
                     {
                         Assert.IsTrue(scope1_1.Parent != null);
 
+                        scope1_1.LogInfo("Logging from scope 1.1");
+
                         using (var scope1_1_1 = new PnPMonitoredScope("1.1.1"))
                         {
                             Assert.IsTrue(scope1_1_1.Parent != null && scope1_1_1.Parent != scope1);
+
+                            scope1_1_1.LogInfo("Logging from scope 1.1.1");
                         }
 
                         using (var scope1_1_2 = new PnPMonitoredScope("1.1.2"))
@@ -36,8 +42,14 @@ namespace OfficeDevPnP.Core.Tests.Diagnostics
                                 Assert.IsTrue(scope1_1_2_1.Parent != null && scope1_1_2_1.Parent == scope1_1_2);
                             }
 
+                            scope1_1_2.LogInfo("Logging from scope 1.1.2");
                         }
                     }
+                }
+
+                using (var topScope2 = new PnPMonitoredScope("1.0"))
+                {
+                    topScope2.LogInfo("Test");
                 }
             }
         }
