82f8b1b9b746afc072b763206e3d513e9ed0bca9
Paolo Pialorsi <paolo@pialorsi.com>
2015-08-22 16:24:30 +0200

    Implemented Workflow types in Domain Model

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Model/Workflows.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Model
+{
+    /// <summary>
+    /// Defines the Workflows to provision
+    /// </summary>
+    public class Workflows
+    {
+        /// <summary>
+        /// Defines the Workflows Definitions to provision
+        /// </summary>
+        public List<WorkflowDefinition> WorkflowDefinitions { get; set; }
+
+        /// <summary>
+        /// Defines the Workflows Subscriptions to provision
+        /// </summary>
+        public List<WorkflowSubscription> WorkflowSubscriptions { get; set; }
+    }
+}
