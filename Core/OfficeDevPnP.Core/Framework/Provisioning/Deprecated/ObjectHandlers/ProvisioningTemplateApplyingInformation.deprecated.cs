0a147475385e6fd745b88139cfb9da8fa219e007
Paolo Pialorsi <paolo@pialorsi.com>
2015-08-31 17:21:52 +0200

    Fixed ProvisioningTemplateApplyingInformation.MessagesDelegate

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Deprecated/ObjectHandlers/ProvisioningTemplateApplyingInformation.deprecated.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
+{
+    public partial class ProvisioningTemplateApplyingInformation
+    {
+        [Obsolete("Please don't use this member, insted use MessagesDelegate")]
+        public ProvisioningMessagesDelegate MessageDelegate
+        {
+            get { return (this.MessagesDelegate); }
+            set { this.MessagesDelegate = value; }
+        }
+    }
+}
