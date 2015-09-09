1f42459b7a679d4573761f21ff4e9a8532a51563
Jānis Veinbergs <JVeinbergs@deac.eu>
2015-09-09 16:48:17 +0300

    Added WindowsTransport authentication method for ADFS to authenticate against OnPrem SharePoint without providing Username/Password

---
+++ Core/OfficeDevPnP.Core/IdentityModel/WSTrustBindings/WindowsWSTrustBinding.cs
@@ -0,0 +1,25 @@
+﻿/* Based on reflectored code coming from Microsoft.IdentityModel.Protocols.WSTrust.Bindings.UserNameWSTrustBinding class */
+
+using System;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace OfficeDevPnP.Core.IdentityModel.WSTrustBindings
+{
+    public class WindowsWSTrustBinding : WSTrustBinding
+    {
+        // Methods
+        public WindowsWSTrustBinding() : base(SecurityMode.Transport) { }
+
+        protected override void ApplyTransportSecurity(HttpTransportBindingElement transport) {
+            transport.AuthenticationScheme = AuthenticationSchemes.Negotiate;
+        }
+
+
+        protected override SecurityBindingElement CreateSecurityBindingElement()
+        {
+            return SecurityBindingElement.CreateKerberosOverTransportBindingElement();
+        }
+   }
+}
