96d3587ff1b5f64b279aea2a33f9f0bc5a966e8c
afsandeberg <marten.afsandeberg@gmail.com>
2015-10-10 08:10:44 +0200

    Just some stuff

---
+++ Core/OfficeDevPnP.Core/Knowit/Search.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SharePoint.Client.Search.Administration;
+using Microsoft.SharePoint.Client.Search.Portability;
+using System;
+using System.Text;
+
+namespace Microsoft.SharePoint.Client
+{
+    /// <summary>
+    /// Class for Search extension methods
+    /// </summary>
+    public static partial class SearchExtensions
+    {
+        /// <summary>
+        /// Returns the current search configuration for the specified object level
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="searchSettingsObjectLevel"></param>
+        /// <returns></returns>
+        public static string GetSearchConfiguration(this ClientContext context, SearchObjectLevel searchSettingsObjectLevel)
+        {
+            return GetSearchConfigurationImplementation(context, searchSettingsObjectLevel);
+        }
+    }
+}
