b9225df711f83ce5dac5b623abc11ed58c342e5c
Erwin van Hunen <erwin.van.hunen@outlook.com>
2015-10-15 16:23:28 +0200

    Updated test attribute

--- Core/OfficeDevPnP.Core.Tests/Framework/Providers/BaseTemplateTests.cs
+++ Core/OfficeDevPnP.Core.Tests/Framework/Providers/BaseTemplateTests.cs
@@ -24,7 +24,7 @@ namespace OfficeDevPnP.Core.Tests.Framework.Providers
         /// This is not a test, merely used to dump the needed template files
         /// </summary>
         [TestMethod]
-        //[Ignore]
+        [Ignore]
         public void DumpBaseTemplates()
         {
             using (ClientContext ctx = TestCommon.CreateClientContext())
