b42f934928362412bbe5ff728e02b5b1f51f18b4
luismanez <lmanez@outlook.com>
2015-11-13 12:22:07 +0000

    Bug fixed. Now the Sequence property is also Updated in the Update Custom Action process

--- Core/OfficeDevPnP.Core/AppModelExtensions/NavigationExtensions.cs
+++ Core/OfficeDevPnP.Core/AppModelExtensions/NavigationExtensions.cs
@@ -641,6 +641,7 @@ namespace Microsoft.SharePoint.Client
             targetAction.Name = customAction.Name;
             targetAction.Description = customAction.Description;
             targetAction.Location = customAction.Location;
+            targetAction.Sequence = customAction.Sequence;
 
             if (customAction.Location == JavaScriptExtensions.SCRIPT_LOCATION)
             {
@@ -648,8 +649,7 @@ namespace Microsoft.SharePoint.Client
                 targetAction.ScriptSrc = customAction.ScriptSrc;
             }
             else
-            {
-                targetAction.Sequence = customAction.Sequence;
+            {                
                 targetAction.Url = customAction.Url;
                 targetAction.Group = customAction.Group;
                 targetAction.Title = customAction.Title;
