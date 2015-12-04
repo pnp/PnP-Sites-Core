4f3778f72a41e4104e65abfee049d39a49697456
Bert Jansen <bjansen@microsoft.com>
2015-12-04 08:14:10 +0100

    Removal of deprecated methods as part of the December master merge process

--- Core/OfficeDevPnP.Core/AppModelExtensions/Deprecated/NavigationExtensions.deprecated.cs
+++ Core/OfficeDevPnP.Core/AppModelExtensions/Deprecated/NavigationExtensions.deprecated.cs
@@ -12,17 +12,5 @@ namespace Microsoft.SharePoint.Client
     /// </summary>
     public static partial class NavigationExtensions
     {
-        #region TO BE DEPRECATED IN DECEMBER 2015 RELEASE
-        /// <summary>
-        /// Deletes all Quick Launch nodes
-        /// </summary>
-        /// <param name="web">Site to be processed - can be root web or sub site</param>
-        [Obsolete("Use DeleteAllNavigationNodes(web, NavigationType.QuickLaunch)")]
-        public static void DeleteAllQuickLaunchNodes(this Web web)
-        {
-            DeleteAllNavigationNodes(web, NavigationType.QuickLaunch);
-        }
-
-        #endregion
     }
 }
