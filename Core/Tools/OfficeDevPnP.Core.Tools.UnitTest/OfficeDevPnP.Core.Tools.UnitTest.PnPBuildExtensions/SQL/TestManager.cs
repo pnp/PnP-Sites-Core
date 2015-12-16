695de636cb30420d4bb2f944acdc0c10a69d13ad
Bert Jansen <bjansen@microsoft.com>
2015-12-16 15:33:16 +0100

    updates

---
+++ Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/SQL/TestManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions.SQL
+{
+    public class TestManager
+    {
+
+
+    }
+}
