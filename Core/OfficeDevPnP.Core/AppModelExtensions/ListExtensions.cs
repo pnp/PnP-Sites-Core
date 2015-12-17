29e8c2ad12d2667be85fbde3e91d675b9c26d83c
Daniel Laskewitz <daniel@laskewitz.nl>
2015-12-17 10:46:22 +0100

    Update to listextensions to add paged property

--- Core/OfficeDevPnP.Core/AppModelExtensions/ListExtensions.cs
+++ Core/OfficeDevPnP.Core/AppModelExtensions/ListExtensions.cs
@@ -927,9 +927,9 @@ namespace Microsoft.SharePoint.Client
         /// <param name="viewFields"></param>
         /// <param name="rowLimit"></param>
         /// <param name="setAsDefault"></param>
-        /// <param name="query"></param>
-        /// <param name="paged"></param>        
+        /// <param name="query"></param>        
         /// <param name="personal"></param>
+        /// <param name="paged"></param>        
         public static View CreateView(this List list,
                                       string viewName,
                                       ViewType viewType,
@@ -937,8 +937,8 @@ namespace Microsoft.SharePoint.Client
                                       uint rowLimit,
                                       bool setAsDefault,
                                       string query = null,
-                                      bool paged = false,
-                                      bool personal = false)
+                                      bool personal = false,
+                                      bool paged = false)
         {
             if (string.IsNullOrEmpty(viewName))
                 throw new ArgumentNullException("viewName");
@@ -949,8 +949,8 @@ namespace Microsoft.SharePoint.Client
             viewCreationInformation.RowLimit = rowLimit;
             viewCreationInformation.ViewFields = viewFields;
             viewCreationInformation.PersonalView = personal;
-            viewCreationInformation.Paged = paged;
             viewCreationInformation.SetAsDefaultView = setAsDefault;
+            viewCreationInformation.Paged = paged;
             if (!string.IsNullOrEmpty(query))
             {
                 viewCreationInformation.Query = query;
