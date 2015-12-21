39d90230ab2b90791595f1487eeb0c29dad3e365
Bert Jansen <bjansen@microsoft.com>
2015-12-21 12:48:57 +0100

    tested in Sql Azure

---
+++ Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/SQL/scripts/createdpnprole.sql
@@ -0,0 +1,6 @@
+﻿create role [PnPLimitedReader]
+
+grant select on dbo.TestRunSet to [PnPLimitedReader]
+grant select on dbo.TestResultSet to [PnPLimitedReader]
+grant select on dbo.TestResultMessageSet to [PnPLimitedReader]
+grant select on dbo.TestConfigurationSet to [PnPLimitedReader]
