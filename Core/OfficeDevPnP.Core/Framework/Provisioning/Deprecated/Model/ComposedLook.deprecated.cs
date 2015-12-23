485cc3358e10762a99a8a95dbca25197752d68bb
Paolo Pialorsi <paolo@pialorsi.com>
2015-12-23 17:24:38 +0100

    Moved SiteLogo and AlternateCSS from ComposedLook to WebSettings, added Title and Description to WebSettings.

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Deprecated/Model/ComposedLook.deprecated.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Model
+{
+    public partial class ComposedLook
+    {
+        /// <summary>
+        /// Gets or sets the Site Logo
+        /// </summary>
+        [Obsolete("Instead of this member, please use SiteLogo property of the WebSettings object.")]
+        public string SiteLogo
+        {
+            get
+            {
+                return null;
+                // return (this.ParentTemplate.WebSettings.SiteLogo);
+            }
+            set
+            {
+                // this.ParentTemplate.WebSettings.SiteLogo = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the AlternateCSS
+        /// </summary>
+        [Obsolete("Instead of this member, please use AlternateCSS property of the WebSettings object.")]
+        public string AlternateCSS
+        {
+            get
+            {
+                return null;
+                // return (this.ParentTemplate.WebSettings.AlternateCSS);
+            }
+            set
+            {
+                // this.ParentTemplate.WebSettings.AlternateCSS = value;
+            }
+        }
+    }
+}
