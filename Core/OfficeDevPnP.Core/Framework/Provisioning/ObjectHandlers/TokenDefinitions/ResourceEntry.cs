60d4666e5ef6edcb6b055b0675618884c318602e
Erwin van Hunen <erwin.van.hunen@outlook.com>
2016-01-27 13:31:03 +0100

    Implemented localization support

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/ResourceEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+    internal class ResourceEntry
+    { 
+        public uint LCID { get; set; }
+        public string Value { get; set; }
+    }
+}
--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenParser.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenParser.cs
@@ -5,6 +5,8 @@ using Microsoft.SharePoint.Client;
 using Microsoft.SharePoint.Client.Taxonomy;
 using OfficeDevPnP.Core.Framework.Provisioning.Model;
 using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions;
+using System.Resources;
+using System.Collections;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 {
@@ -13,6 +15,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
         public Web _web;
 
         private List<TokenDefinition> _tokens = new List<TokenDefinition>();
+        private List<Localization> _localizations = new List<Localization>();
 
         public List<TokenDefinition> Tokens
         {
@@ -37,7 +40,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
         public TokenParser(Web web, ProvisioningTemplate template)
         {
-            web.EnsureProperties(w => w.ServerRelativeUrl);
+            web.EnsureProperties(w => w.ServerRelativeUrl, w => w.Language);
 
             _web = web;
 
@@ -109,6 +112,41 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             _tokens.Add(new SiteCollectionTermGroupIdToken(web));
             _tokens.Add(new SiteCollectionTermGroupNameToken(web));
 
+            // Handle resources
+            if (template.Localizations.Any())
+            {
+                // Read all resource keys in a list
+                List<Tuple<string, uint, string>> resourceEntries = new List<Tuple<string, uint, string>>();
+                foreach (var localizationEntry in template.Localizations)
+                {
+                    var filePath = localizationEntry.ResourceFile;
+                    using (var stream = template.Connector.GetFileStream(filePath))
+                    {
+                        if (stream != null)
+                        {
+                            using (ResXResourceReader resxReader = new ResXResourceReader(stream))
+                            {
+                                foreach (DictionaryEntry entry in resxReader)
+                                {
+                                    resourceEntries.Add(new Tuple<string, uint, string>(entry.Key.ToString(), (uint)localizationEntry.LCID, entry.Value.ToString()));
+                                }
+                            }
+                        }
+                    }
+                }
+
+                var uniqueKeys = resourceEntries.Select(k => k.Item1).Distinct();
+                foreach (var key in uniqueKeys)
+                {
+                    var matches = resourceEntries.Where(k => k.Item1 == key);
+                    var entries = matches.Select(k => new ResourceEntry() { LCID = k.Item2, Value = k.Item3 }).ToList();
+                    LocalizationToken token = new LocalizationToken(web, key, entries);
+
+                    _tokens.Add(token);
+                }
+                
+
+            }
             var sortedTokens = from t in _tokens
                                orderby t.GetTokenLength() descending
                                select t;
@@ -118,6 +156,8 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
         public void Rebase(Web web)
         {
+            web.EnsureProperties(w => w.ServerRelativeUrl, w => w.Language);
+
             _web = web;
 
             foreach (var token in _tokens)
@@ -195,6 +235,5 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
             return input;
         }
-
     }
 }
