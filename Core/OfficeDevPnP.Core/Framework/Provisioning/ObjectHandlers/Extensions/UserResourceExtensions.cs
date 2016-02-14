e1505d5015537a9bf3716c9ded3fc4fa490e6148
Erwin van Hunen <erwin.van.hunen@outlook.com>
2016-02-14 16:41:40 +0100

    optimized ContainsResourceToken test

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Extensions/UserResourceExtensions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
@@ -31,16 +32,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Extensions
         {
             if (value != null)
             {
-                value = value.ToLower();
-                return value.IndexOf("{res:") > -1 ||
-                    value.IndexOf("{loc:") > -1 ||
-                    value.IndexOf("{resource:") > -1 ||
-                    value.IndexOf("{localize:") > -1 ||
-                    value.IndexOf("{localization:") > -1;
+                return Regex.IsMatch(value, "\\{(res|loc|resource|localize|localization):(.*?)(\\})", RegexOptions.IgnoreCase);
             }
             else
             {
-                return (false);
+                return false;
             }
         }
     }
