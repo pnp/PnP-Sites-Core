5afd94045111f402fb4ae42cffd3bff71acee25a
Bert Jansen <bjansen@microsoft.com>
2016-05-03 13:35:46 +0200

    small change to also allow the pnp user to be used to login via SSMS

--- Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/SQL/scripts/createuserandlogin.sql
+++ Core/Tools/OfficeDevPnP.Core.Tools.UnitTest/OfficeDevPnP.Core.Tools.UnitTest.PnPBuildExtensions/SQL/scripts/createuserandlogin.sql
@@ -2,6 +2,9 @@
 create login PnP with password = 'pwd'
 create login PnPReader with password = 'pwd'
 
+-- in master db (needed to allow PnP user to login SSMS)
+CREATE USER [PnP] FOR LOGIN [PnP] WITH DEFAULT_SCHEMA = dbo
+
 -- in PnP db
 CREATE USER [PnP] FOR LOGIN [PnP] WITH DEFAULT_SCHEMA = dbo
 CREATE USER [PnPReader]	FOR LOGIN [PnPReader] WITH DEFAULT_SCHEMA = dbo
