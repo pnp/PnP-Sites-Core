a875e243ca39552a4bb6873c8dd09523793f7f73
Erwin van Hunen <erwin.van.hunen@outlook.com>
2016-06-02 12:06:24 +0200

    Refactored/simplified EnableResponsiveUI extension method

--- Core/OfficeDevPnP.Core/CoreResources.Designer.cs
+++ Core/OfficeDevPnP.Core/CoreResources.Designer.cs
@@ -507,80 +507,6 @@ namespace OfficeDevPnP.Core {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to var headID = document.getElementsByTagName(&apos;head&apos;)[0];               
-        ///var newScript = document.createElement(&apos;script&apos;);               
-        ///newScript.type = &apos;text/javascript&apos;;
-        ///newScript.src = &apos;{InfrastructureSiteUrl}/Style%20Library/SP.Responsive.UI/{ScriptName}?rev=bf19e4f64b204e1ebc2f762e33afcc97&apos;;
-        ///newScript.id = &apos;PnPResponsiveUI&apos;;
-        ///headID.appendChild(newScript);.
-        /// </summary>
-        internal static string PnP_Responsive_CustomActionScript {
-            get {
-                return ResourceManager.GetString("PnP_Responsive_CustomActionScript", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to .mobile-only {
-        ///    display: none;
-        ///}
-        ///
-        ///#sideNavBox, #contentRow, #siteIcon {
-        ///    -webkit-transition: all 400ms ease;
-        ///    -moz-transition: all 400ms ease;
-        ///    -ms-transition: all 400ms ease;
-        ///    -o-transition: all 400ms ease;
-        ///    transition: all 400ms ease;
-        ///}
-        ///
-        ///#contentBox {
-        ///	min-width: auto;
-        ///}
-        ///
-        ////* Make sure dialog windows don&apos;t break */
-        ///.ms-dialog #contentRow {
-        ///    margin-left: 0;
-        ///}
-        ///
-        ///.ms-dialog #contentBox {
-        ///    margin-top: 0;
-        ///}
-        ///
-        ///.ms-dialog #s4-bodyContainer {
-        ///    min-width: 0;
-        ///    f [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string PnP_Responsive_UI_CSS {
-            get {
-                return ResourceManager.GetString("PnP_Responsive_UI_CSS", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to /* PnP SharePoint - Responsiveness */
-        ///
-        ///var PnPResponsiveApp = PnPResponsiveApp || {};
-        ///
-        ///PnPResponsiveApp.responsivizeSettings = function () {
-        ///	// return if no longer on Settings page
-        ///	if (window.location.href.indexOf(&apos;/settings.aspx&apos;) &lt; 0) return;
-        ///	
-        ///	// find the Settings root element, or wait if not available yet
-        ///	var settingsRoot = $(&quot;.ms-siteSettings-root&quot;);
-        ///	if (!settingsRoot.length) {
-        ///		setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
-        ///        return;
-        ///	}
-        ///	
-        ///	$(&quot;.ms-siteSettings-root . [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string PnP_Responsive_UI_JS {
-            get {
-                return ResourceManager.GetString("PnP_Responsive_UI_JS", resourceCulture);
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized string similar to Code execution scope ended.
         /// </summary>
         internal static string PnPMonitoredScope_Code_execution_ended {
@@ -1831,6 +1757,66 @@ namespace OfficeDevPnP.Core {
         internal static string SiteToTemplateConversion_ProgressDelegate_registered {
             get {
                 return ResourceManager.GetString("SiteToTemplateConversion_ProgressDelegate_registered", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to /* PnP SharePoint - Responsiveness */
+        ///
+        ///var PnPResponsiveApp = PnPResponsiveApp || {};
+        ///
+        ///PnPResponsiveApp.responsivizeSettings = function () {
+        ///	// return if no longer on Settings page
+        ///	if (window.location.href.indexOf(&apos;/settings.aspx&apos;) &lt; 0) return;
+        ///	
+        ///	// find the Settings root element, or wait if not available yet
+        ///	var settingsRoot = $(&quot;.ms-siteSettings-root&quot;);
+        ///	if (!settingsRoot.length) {
+        ///		setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
+        ///        return;
+        ///	}
+        ///	
+        ///	$(&quot;.ms-siteSettings-root . [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SP_Responsive_UI {
+            get {
+                return ResourceManager.GetString("SP_Responsive_UI", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .mobile-only {
+        ///    display: none;
+        ///}
+        ///
+        ///#sideNavBox, #contentRow, #siteIcon {
+        ///    -webkit-transition: all 400ms ease;
+        ///    -moz-transition: all 400ms ease;
+        ///    -ms-transition: all 400ms ease;
+        ///    -o-transition: all 400ms ease;
+        ///    transition: all 400ms ease;
+        ///}
+        ///
+        ///#contentBox {
+        ///	min-width: auto;
+        ///}
+        ///
+        ////* Make sure dialog windows don&apos;t break */
+        ///.ms-dialog #contentRow {
+        ///    margin-left: 0;
+        ///}
+        ///
+        ///.ms-dialog #contentBox {
+        ///    margin-top: 0;
+        ///}
+        ///
+        ///.ms-dialog #s4-bodyContainer {
+        ///    min-width: 0;
+        ///    f [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SP_Responsive_UI_CSS {
+            get {
+                return ResourceManager.GetString("SP_Responsive_UI_CSS", resourceCulture);
             }
         }
         
--- Core/OfficeDevPnP.Core/Resources/PnP-Responsive-CustomActionScript.js
+++
@@ -1,6 +0,0 @@
-var headID = document.getElementsByTagName('head')[0];               
-var newScript = document.createElement('script');               
-newScript.type = 'text/javascript';
-newScript.src = '{InfrastructureSiteUrl}/Style%20Library/SP.Responsive.UI/{ScriptName}?rev=bf19e4f64b204e1ebc2f762e33afcc97';
-newScript.id = 'PnPResponsiveUI';
-headID.appendChild(newScript);
