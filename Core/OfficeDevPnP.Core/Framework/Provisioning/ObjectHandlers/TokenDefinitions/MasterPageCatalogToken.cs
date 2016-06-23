f75f44a20f3cfbd1afdc308e03a5d23da4ea7aef
Elio Struyf <info@estruyf.be>
2016-06-23 10:41:14 +0200

    Master page URL token fix when provisioning template to a sub-site.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/MasterPageCatalogToken.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/MasterPageCatalogToken.cs
@@ -13,7 +13,18 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitio
         {
             if (this.CacheValue == null)
             {
-                var catalog = Web.GetCatalog((int) ListTemplateType.MasterPageCatalog);
+                List catalog;
+                // Check if the current web is a sub-site
+                if (Web.IsSubSite())
+                {
+                    // Master page URL needs to be retrieved from the rootweb
+                    var rootWeb = (Web.Context as ClientContext).Site.RootWeb;
+                    catalog = rootWeb.GetCatalog((int)ListTemplateType.MasterPageCatalog);
+                }
+                else
+                {
+                    catalog = Web.GetCatalog((int)ListTemplateType.MasterPageCatalog);
+                }
                 Web.Context.Load(catalog, c => c.RootFolder.ServerRelativeUrl);
                 Web.Context.ExecuteQueryRetry();
                 CacheValue = catalog.RootFolder.ServerRelativeUrl;
