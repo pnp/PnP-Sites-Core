c99378d8497d34b77dc16c4d4fa3d1010d9ada80
Edwin Lunandy <elunandy@newsignature.com>
2016-08-31 10:41:26 -0400

    Fix missing semicolon. Declare multiple variables with a single comma-delimited 'var' for better readability.

--- Core/OfficeDevPnP.Core/Resources/SP-Responsive-UI.js
+++ Core/OfficeDevPnP.Core/Resources/SP-Responsive-UI.js
@@ -1,28 +1,27 @@
 /* PnP SharePoint - Responsiveness */
-
 var PnPResponsiveApp = PnPResponsiveApp || {};
 
 PnPResponsiveApp.responsivizeSettings = function () {
-	// return if no longer on Settings page
-	if (window.location.href.indexOf('/settings.aspx') < 0) return;
-	
-	// find the Settings root element, or wait if not available yet
-	var settingsRoot = $(".ms-siteSettings-root");
-	if (!settingsRoot.length) {
-		setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
+    // return if no longer on Settings page
+    if (window.location.href.indexOf('/settings.aspx') < 0) return;
+
+    // find the Settings root element, or wait if not available yet
+    var settingsRoot = $(".ms-siteSettings-root");
+    if (!settingsRoot.length) {
+        setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
         return;
-	}
-	
-	$(".ms-siteSettings-root .ms-linksection-level1").each(function () {
-		var self = $(this);
-		var settingsDiv = $('<div>');
-		settingsDiv.addClass("pnp-settingsdiv");
-		self.find(".ms-linksection-iconCell img").appendTo(settingsDiv);
-		self.find(".ms-linksection-textCell").children().appendTo(settingsDiv);
-		settingsDiv.appendTo(settingsRoot);
-	});
-	settingsRoot.find("table").remove();
-}
+    }
+
+    $(".ms-siteSettings-root .ms-linksection-level1").each(function () {
+        var self = $(this),
+            settingsDiv = $('<div>');
+        settingsDiv.addClass("pnp-settingsdiv");
+        self.find(".ms-linksection-iconCell img").appendTo(settingsDiv);
+        self.find(".ms-linksection-textCell").children().appendTo(settingsDiv);
+        settingsDiv.appendTo(settingsRoot);
+    });
+    settingsRoot.find("table").remove();
+};
 
 
 PnPResponsiveApp.setUpToggling = function () {
@@ -31,8 +30,8 @@ PnPResponsiveApp.setUpToggling = function () {
         return;
 
     // Set up sidenav toggling
-    var topNav = $('#DeltaTopNavigation');
-    var topNavClone = topNav.clone()
+    var topNav = $('#DeltaTopNavigation'),
+        topNavClone = topNav.clone();
     topNavClone.addClass('mobile-only');
     topNavClone.attr('id', topNavClone.attr('id') + "_mobileClone");
     topNav.addClass('no-mobile');
@@ -45,17 +44,17 @@ PnPResponsiveApp.setUpToggling = function () {
 	sideNavToggle.html("<span></span>");
     sideNavToggle.click(function() { 
         $("body").toggleClass('shownav');
-		sideNavToggle.toggleClass('selected');
+        $(this).toggleClass('selected');
     });
     $("#pageTitle").before(sideNavToggle);
-}
+};
 
 PnPResponsiveApp.init = function () {
     if (!window.jQuery) {
         // jQuery is needed for PnP Responsive UI to run, and is not fully loaded yet, try later
         setTimeout(PnPResponsiveApp.init, 100);
     } else {
-        $(function() { // only execute when DOM is fully loaded
+        $(function () { // only execute when DOM is fully loaded
 
             // embedding and loading of all necessary CSS files and JS libraries
             var currentScriptUrl = $('#PnPResponsiveUI').attr('src');
@@ -82,31 +81,33 @@ PnPResponsiveApp.init = function () {
 			}
         });
     }
-}
+};
 
 /* Dynamic CSS/JS embedding and loading */
 function loadCSS(url) {
-    var head = document.getElementsByTagName('head')[0];
-    var style = document.createElement('link');
-	style.type = 'text/css';
-	style.rel = 'stylesheet';
+    var head = document.getElementsByTagName('head')[0],
+        style = document.createElement('link');
+    style.type = 'text/css';
+    style.rel = 'stylesheet';
     style.href = url;
     head.appendChild(style);
 }
+
 function loadScript(url, callback) {
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
+    var head = document.getElementsByTagName('head')[0],
+        script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
     script.onreadystatechange = callback;
     script.onload = callback;
     head.appendChild(script);
 }
+
 function addViewport() {
-    var head = document.getElementsByTagName('head')[0];
-    var viewport = document.createElement('meta');
-    viewport.name= "viewport";
-    viewport.content= "width=device-width, initial-scale=1"; 
+    var head = document.getElementsByTagName('head')[0],
+        viewport = document.createElement('meta');
+    viewport.name = "viewport";
+    viewport.content = "width=device-width, initial-scale=1";
     head.appendChild(viewport);
 }
 
