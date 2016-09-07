90c8332f21ce87d2cc052116eea9c3843cd8ec0c
Michael Rees Pullen <mrpullen@outlook.com>
2016-09-06 22:39:25 -0400

    Updated to allow for ADFS Certificate Authentication

---
+++ Core/OfficeDevPnP.Core/IdentityModel/WSTrustBindings/CertificateWSTrustBinding.cs
@@ -0,0 +1,108 @@
+/* Based on reflectored code coming from Microsoft.IdentityModel.Protocols.WSTrust.Bindings.UserNameWSTrustBinding class */
+
+using System;
+using System.Net;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+namespace OfficeDevPnP.Core.IdentityModel.WSTrustBindings
+{
+    public class CertificateWSTrustBinding : WSTrustBinding
+    {
+        // Fields
+        private HttpClientCredentialType _clientCredentialType;
+
+        // Methods
+        public CertificateWSTrustBinding() : this(SecurityMode.Message, HttpClientCredentialType.None)
+        {
+        }
+
+        public CertificateWSTrustBinding(SecurityMode securityMode) : base(securityMode)
+        {
+            if (SecurityMode.Message == securityMode)
+            {
+                _clientCredentialType = HttpClientCredentialType.None;
+            }
+        }
+
+        public CertificateWSTrustBinding(SecurityMode mode, HttpClientCredentialType clientCredentialType) : base(mode)
+        {
+            if (!IsHttpClientCredentialTypeDefined(clientCredentialType))
+            {
+                throw new ArgumentOutOfRangeException("clientCredentialType");
+            }
+
+            if (((SecurityMode.Transport == mode) && (HttpClientCredentialType.Digest != clientCredentialType)) && (HttpClientCredentialType.Basic != clientCredentialType))
+            {
+                throw new InvalidOperationException("ID3225");
+            }
+
+            _clientCredentialType = clientCredentialType;
+        }
+
+        protected override void ApplyTransportSecurity(HttpTransportBindingElement transport)
+        {
+            if (_clientCredentialType == HttpClientCredentialType.Certificate)
+            {
+                transport.AuthenticationScheme = AuthenticationSchemes.Negotiate;
+            }
+            else
+            {
+                transport.AuthenticationScheme = AuthenticationSchemes.Negotiate;
+            }
+        }
+
+        protected override SecurityBindingElement CreateSecurityBindingElement()
+        {
+            if (SecurityMode.Message == base.SecurityMode)
+            {
+                return SecurityBindingElement.CreateCertificateSignatureBindingElement();
+            }
+
+            if (SecurityMode.Transport == base.SecurityMode)
+            {
+                //return SecurityBindingElement.CreateUserNameOverTransportBindingElement();
+                return SecurityBindingElement.CreateCertificateOverTransportBindingElement();
+            }
+
+            if (SecurityMode.TransportWithMessageCredential == base.SecurityMode)
+            {
+                return SecurityBindingElement.CreateCertificateOverTransportBindingElement();
+            }
+
+            return null;
+        }
+
+        private static bool IsHttpClientCredentialTypeDefined(HttpClientCredentialType value)
+        {
+            if ((((value != HttpClientCredentialType.None) && (value != HttpClientCredentialType.Basic)) && ((value != HttpClientCredentialType.Digest) && (value != HttpClientCredentialType.Ntlm))) && (value != HttpClientCredentialType.Windows))
+            {
+                return (value == HttpClientCredentialType.Certificate);
+            }
+
+            return true;
+        }
+
+        // Properties
+        public HttpClientCredentialType ClientCredentialType
+        {
+            get
+            {
+                return _clientCredentialType;
+            }
+            set
+            {
+                if (!IsHttpClientCredentialTypeDefined(value))
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                if (((SecurityMode.Transport == base.SecurityMode) && (HttpClientCredentialType.Digest != value)) && (HttpClientCredentialType.Basic != value))
+                {
+                    throw new InvalidOperationException("ID3225");
+                }
+                _clientCredentialType = value;
+            }
+        }
+    }
+}
+
