f6392fb0f764093aeadf537a0e88ab99779dd416
srinutamada <srinu.tamada@gmail.com>
2016-09-13 13:58:20 -0700

    Pages, File and Workflow Validation Handler
    Implemented pages, files and workflow related handlers

---
+++ Core/OfficeDevPnP.Core.Tests/Framework/Functional/Validators/FilesValidator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Tests.Framework.Functional.Validators;
+using System.Collections.Generic;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using System.Xml;
+using System.Xml.Linq;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using System.Linq;
+using OfficeDevPnP.Core.Utilities;
+
+namespace OfficeDevPnP.Core.Tests.Framework.Functional.Validators
+{
+    [TestClass]
+    public class FilesValidator : ValidatorBase
+    {
+        public FilesValidator() : base()
+        {
+            // optionally override schema version
+            SchemaVersion = XMLConstants.PROVISIONING_SCHEMA_NAMESPACE_2016_05;
+        }
+
+        public bool Validate(FileCollection sourceFiles, Microsoft.SharePoint.Client.ClientContext ctx)
+        {
+            int scount = 0;
+            int tcount = 0;
+
+            foreach (var sf in sourceFiles)
+            {
+                scount++;
+                string fileName = sf.Src;
+                string folderName = sf.Folder;
+                string fileUrl = UrlUtility.Combine(ctx.Web.ServerRelativeUrl, folderName + "/" + fileName);
+                var file = ctx.Web.GetFileByServerRelativeUrl(UrlUtility.Combine(ctx.Web.ServerRelativeUrl, folderName + "/" + fileName));
+                ctx.Load(file, f => f.Exists);
+                ctx.ExecuteQuery();
+
+                if (file.Exists)
+                {
+                    tcount++;
+
+                    #region File - Security
+                    if (sf.Security != null)
+                    {
+                        ctx.Load(file, f => f.ListItemAllFields);
+                        ctx.ExecuteQuery();
+                        bool isSecurityMatch = ValidateSecurityCSOM(ctx, sf.Security, file.ListItemAllFields);
+                        if (!isSecurityMatch)
+                        {
+                            return false;
+                        }
+
+                    }
+                    #endregion
+                    #region Webparts validation
+                    if (sf.WebParts.Count > 0)
+                    {
+                        WebpartValidator wpv = new WebpartValidator();
+                        bool isWepartMatch = wpv.Validate(ctx, sf, file);
+                        if (!isWepartMatch)
+                        {
+                            return false;
+                        }
+                    }
+                    #endregion
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Core/OfficeDevPnP.Core.Tests/Framework/Functional/WorkflowsTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.SharePoint.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Tests.Framework.Functional.Validators;
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace OfficeDevPnP.Core.Tests.Framework.Functional
+{
+    [TestClass]
+    public class WorkflowsTests : FunctionalTestBase
+    {
+        #region Construction
+        public WorkflowsTests()
+        {
+            //debugMode = true;
+            centralSiteCollectionUrl = "https://crtlab2.sharepoint.com/sites/source2";
+            centralSubSiteUrl = "https://crtlab2.sharepoint.com/sites/source2/sub2";
+        }
+        #endregion
+
+        #region Test setup
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            //ClassInitBase(context);
+        }
+
+        [ClassCleanup()]
+        public static void ClassCleanup()
+        {
+            //ClassCleanupBase();
+        }
+        #endregion
+
+        #region Site collection test cases
+        /// <summary>
+        /// WorkflowsTests Test
+        /// </summary>
+        [TestMethod]
+        public void WorkflowsTest()
+        {
+            using (var cc = TestCommon.CreateClientContext(centralSiteCollectionUrl))
+            {
+                ProvisioningTemplateCreationInformation ptci = new ProvisioningTemplateCreationInformation(cc.Web);
+                ptci.HandlersToProcess = Handlers.Workflows;
+                ptci.FileConnector= new FileSystemConnector(string.Format(@"{0}\..\..\Framework\Functional", AppDomain.CurrentDomain.BaseDirectory), "Templates");
+
+
+                var result = TestProvisioningTemplate(cc, "workflows_add.xml", Handlers.Workflows,null,ptci);
+                WorkflowValidator wv = new WorkflowValidator();
+                Assert.IsTrue(wv.Validate(result.SourceTemplate.Workflows,result.TargetTemplate.Workflows,result.TargetTokenParser));
+            }
+        }
+        #endregion
+    }
+}
