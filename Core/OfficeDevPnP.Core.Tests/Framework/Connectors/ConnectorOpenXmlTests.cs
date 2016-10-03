46721c64d5cd9a66073a8aba17fc3f1662cffe84
Ivan vagunin <ivan.vagunin@gmail.com>
2016-10-03 10:48:34 -0700

    open xml failing test

--- Core/OfficeDevPnP.Core.Tests/Framework/Connectors/ConnectorOpenXmlTests.cs
+++ Core/OfficeDevPnP.Core.Tests/Framework/Connectors/ConnectorOpenXmlTests.cs
@@ -11,6 +11,7 @@ namespace OfficeDevPnP.Core.Tests.Framework.Connectors
 	{
 		private const string packageFileName = "TestTemplate.pnp";
 		private const string packageFileNameBackwardsCompatibility = "TestTemplateOriginal.pnp";
+		private const string packageFileNameBackwardsCompatibility2 = "TestTemplateOriginal2.pnp";
 
 		#region Test initialize and cleanup
 
@@ -80,6 +81,30 @@ namespace OfficeDevPnP.Core.Tests.Framework.Connectors
 					"Templates");
 
 			var openXMLConnector = new OpenXMLConnector(packageFileNameBackwardsCompatibility, fileSystemConnector);
+			var templateFile = openXMLConnector.GetFileStream("ProvisioningSchema-2015-12-FullSample-02.xml");
+
+			XMLPnPSchemaV201512Formatter formatter = new XMLPnPSchemaV201512Formatter();
+			Boolean checkTemplate = formatter.IsValid(templateFile);
+
+			Assert.IsTrue(checkTemplate);
+
+			var image1 = openXMLConnector.GetFileStream("garagelogo.png", "Images");
+			Assert.IsNotNull(image1);
+
+			var image2 = openXMLConnector.GetFileStream("garagebg.jpg", "Images");
+			Assert.IsNotNull(image2);
+		}
+
+
+		[TestMethod]
+		public void OpenXMLLoadTemplateOriginal2()
+		{
+			var fileSystemConnector = new FileSystemConnector(
+					String.Format(@"{0}\..\..\Resources",
+					AppDomain.CurrentDomain.BaseDirectory),
+					"Templates");
+
+			var openXMLConnector = new OpenXMLConnector(packageFileNameBackwardsCompatibility2, fileSystemConnector);
 			var templateFile = openXMLConnector.GetFileStream("ProvisioningSchema-2015-12-FullSample-02.xml");
 
 			XMLPnPSchemaV201512Formatter formatter = new XMLPnPSchemaV201512Formatter();
