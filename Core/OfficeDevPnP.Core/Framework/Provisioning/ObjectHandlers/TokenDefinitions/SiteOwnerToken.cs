1e7f670baedce48f3fd712e5306876e7f1faa442
SiobhanBaynes <siobhanbaynes@outlook.com>
2016-10-14 10:13:44 +0100

    Add {SiteOwnerToken} to provisioning tokens

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/SiteOwnerToken.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.SharePoint.Client;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+	internal class SiteOwnerToken : TokenDefinition
+	{
+		public SiteOwnerToken(Web web)
+			: base(web, "~siteowner", "{siteowner}")
+		{
+		}
+
+		public override string GetReplaceValue()
+		{
+			if (CacheValue == null)
+			{
+				var context = Web.Context as ClientContext;
+				var site = context.Site;
+				context.Load(site, s => s.Owner);
+				context.ExecuteQueryRetry();
+				CacheValue = site.Owner.LoginName;
+			}
+			return CacheValue;
+		}
+	}
+}
