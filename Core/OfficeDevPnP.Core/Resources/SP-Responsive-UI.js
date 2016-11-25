6ac685fe49d12186ebd43866f6e5cf6657e16f7b
Paolo Pialorsi <paolo@pialorsi.com>
2016-11-25 16:02:22 +0100

    Fixed some missing semicolons in code.

--- Core/OfficeDevPnP.Core/Resources/SP-Responsive-UI.js
+++ Core/OfficeDevPnP.Core/Resources/SP-Responsive-UI.js
@@ -3,59 +3,58 @@
 var PnPResponsiveApp = PnPResponsiveApp || {};
 
 PnPResponsiveApp.responsivizeSettings = function () {
-	// return if no longer on Settings page
-	if (window.location.href.indexOf('/settings.aspx') < 0) return;
-	
-	// find the Settings root element, or wait if not available yet
-	var settingsRoot = $(".ms-siteSettings-root");
-	if (!settingsRoot.length) {
-		setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
-        return;
-	}
-	
-	$(".ms-siteSettings-root .ms-linksection-level1").each(function () {
-		var self = $(this);
-		var settingsDiv = $('<div>');
-		settingsDiv.addClass("pnp-settingsdiv");
-		self.find(".ms-linksection-iconCell img").appendTo(settingsDiv);
-		self.find(".ms-linksection-textCell").children().appendTo(settingsDiv);
-		settingsDiv.appendTo(settingsRoot);
-	});
-	settingsRoot.find("table").remove();
-}
+    // return if no longer on Settings page
+    if (window.location.href.indexOf('/settings.aspx') < 0) return;
 
+    // find the Settings root element, or wait if not available yet
+    var settingsRoot = $(".ms-siteSettings-root");
+    if (!settingsRoot.length) {
+        setTimeout(PnPResponsiveApp.responsivizeSettings, 100);
+        return;
+    }
+
+    $(".ms-siteSettings-root .ms-linksection-level1").each(function () {
+        var self = $(this);
+        var settingsDiv = $('<div>');
+        settingsDiv.addClass("pnp-settingsdiv");
+        self.find(".ms-linksection-iconCell img").appendTo(settingsDiv);
+        self.find(".ms-linksection-textCell").children().appendTo(settingsDiv);
+        settingsDiv.appendTo(settingsRoot);
+    });
+    settingsRoot.find("table").remove();
+};
 
 PnPResponsiveApp.setUpToggling = function () {
-	// if it is already responsivized, return
+    // if it is already responsivized, return
     if ($("#navbar-toggle").length)
         return;
 
     // Set up sidenav toggling
     var topNav = $('#DeltaTopNavigation');
-    var topNavClone = topNav.clone()
+    var topNavClone = topNav.clone();
     topNavClone.addClass('mobile-only');
     topNavClone.attr('id', topNavClone.attr('id') + "_mobileClone");
     topNav.addClass('no-mobile');
     $('#sideNavBox').append(topNavClone);
     var sideNavToggle = $('<button>');
-    sideNavToggle.attr('id', 'navbar-toggle')
+    sideNavToggle.attr('id', 'navbar-toggle');
     sideNavToggle.addClass('mobile-only');
-	sideNavToggle.addClass('burger');
+    sideNavToggle.addClass('burger');
     sideNavToggle.attr('type', 'button');
-	sideNavToggle.html("<span></span>");
-    sideNavToggle.click(function() { 
+    sideNavToggle.html("<span></span>");
+    sideNavToggle.click(function () {
         $("body").toggleClass('shownav');
-		sideNavToggle.toggleClass('selected');
+        sideNavToggle.toggleClass('selected');
     });
     $("#pageTitle").before(sideNavToggle);
-}
+};
 
 PnPResponsiveApp.init = function () {
     if (!window.jQuery) {
         // jQuery is needed for PnP Responsive UI to run, and is not fully loaded yet, try later
         setTimeout(PnPResponsiveApp.init, 100);
     } else {
-        $(function() { // only execute when DOM is fully loaded
+        $(function () { // only execute when DOM is fully loaded
 
             // embedding and loading of all necessary CSS files and JS libraries
             var currentScriptUrl = $('#PnPResponsiveUI').attr('src');
@@ -67,22 +66,26 @@ PnPResponsiveApp.init = function () {
             }
 
             PnPResponsiveApp.setUpToggling();
-			PnPResponsiveApp.responsivizeSettings();
-			
-			// also listen for dynamic page change to Settings page
-			window.onhashchange = function () { PnPResponsiveApp.responsivizeSettings(); };
-			
-			// extend/override some SP native functions to fix resizing quirks
-			var originalResizeFunction = FixRibbonAndWorkspaceDimensions;
-			FixRibbonAndWorkspaceDimensions = function() {
-				// let sharepoint do its thing
-				originalResizeFunction();
-				// fix the body container width
-				$("#s4-bodyContainer").width($("#s4-workspace").width() );
-			}
+            PnPResponsiveApp.responsivizeSettings();
+
+            // also listen for dynamic page change to Settings page
+            window.onhashchange = function () { PnPResponsiveApp.responsivizeSettings(); };
+
+            // Extend/override some SP native functions to fix resizing quirks
+
+            // First of all save the original function definition
+            var originalResizeFunction = FixRibbonAndWorkspaceDimensions;
+
+            // Then define a new one
+            FixRibbonAndWorkspaceDimensions = function () {
+                // let sharepoint do its thing
+                originalResizeFunction();
+                // fix the body container width
+                $("#s4-bodyContainer").width($("#s4-workspace").width());
+            };
         });
     }
-}
+};
 
 /* Dynamic CSS/JS embedding and loading */
 function loadCSS(url) {
