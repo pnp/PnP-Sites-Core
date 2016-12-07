4ac31481bcb5e91937535bf14fa50a81e5ec1a00
Paolo Pialorsi <paolo@pialorsi.com>
2016-12-07 23:38:51 +0100

    ListViewId implementation

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/ListViewIdToken.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.SharePoint.Client;
+using System.Text.RegularExpressions;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+    internal class ListViewIdToken : TokenDefinition
+    {
+        private string _listTitle = null;
+        private string _viewTitle = null;
+
+        public ListViewIdToken(Web web, string listTitle, string viewTitle)
+            : base(web, string.Format("{{viewid:{0},{1}}}",
+                Regex.Escape(listTitle),
+                Regex.Escape(viewTitle)))
+        {
+            _listTitle = listTitle;
+            _viewTitle = viewTitle;
+        }
+
+        public override string GetReplaceValue()
+        {
+            if (CacheValue == null)
+            {
+                using (ClientContext context = this.Web.Context.Clone(this.Web.Url))
+                {
+                    var list = this.Web.Lists.GetByTitle(_listTitle);
+                    var view = list.Views.GetByTitle(_viewTitle);
+                    context.Load(view, v => v.Id);
+                    context.ExecuteQueryRetry();
+                    CacheValue = view.Id.ToString();
+                }
+            }
+            return CacheValue;
+        }
+    }
+}
