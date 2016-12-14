1aebe1a7723311da17b002b491a3ebada199a6c8
Atul Moghe <atul.moghe@gmail.com>
2016-12-14 19:30:48 +0530

    Fixed the directory recursion issue.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectFiles.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectFiles.cs
@@ -530,11 +530,17 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             if (directory.Recursive)
             {
                 var subFolders = directory.ParentTemplate.Connector.GetFolders(directory.Src);
+                var parentFolder = directory;
                 foreach (var folder in subFolders)
                 {
-                    directory.Src += @"\" + folder;
-                    directory.Folder += @"\" + folder;
+                    directory.Src = parentFolder.Src + @"\" + folder;
+                    directory.Folder = parentFolder.Folder + @"\" + folder;
+
                     result.AddRange(directory.GetDirectoryFiles(metadataProperties));
+
+                    //Remove the subfolder path(added above) as the second subfolder should come under its parent folder and not under its sibling
+                    parentFolder.Src = parentFolder.Src.Substring(0, parentFolder.Src.LastIndexOf(@"\"));
+                    parentFolder.Folder = parentFolder.Folder.Substring(0, parentFolder.Folder.LastIndexOf(@"\"));
                 }
             }
 
