ae388927cbcbd19a67b2344e129cdf468a0e561b
Paolo Pialorsi <paolo@pialorsi.com>
2017-01-03 17:19:14 +0100

    Added support for ListViewId token.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/ListViewIdToken.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/ListViewIdToken.cs
@@ -1,34 +1,26 @@
 ﻿using Microsoft.SharePoint.Client;
+using System;
 using System.Text.RegularExpressions;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
 {
     internal class ListViewIdToken : TokenDefinition
     {
-        private string _listTitle = null;
-        private string _viewTitle = null;
+        private string _viewId = null;
 
-        public ListViewIdToken(Web web, string listTitle, string viewTitle)
+        public ListViewIdToken(Web web, string listTitle, string viewTitle, Guid viewId)
             : base(web, string.Format("{{viewid:{0},{1}}}",
                 Regex.Escape(listTitle),
                 Regex.Escape(viewTitle)))
         {
-            _listTitle = listTitle;
-            _viewTitle = viewTitle;
+            _viewId = viewId.ToString();
         }
 
         public override string GetReplaceValue()
         {
-            if (CacheValue == null)
+            if (string.IsNullOrEmpty(CacheValue))
             {
-                using (ClientContext context = this.Web.Context.Clone(this.Web.Url))
-                {
-                    var list = this.Web.Lists.GetByTitle(_listTitle);
-                    var view = list.Views.GetByTitle(_viewTitle);
-                    context.Load(view, v => v.Id);
-                    context.ExecuteQueryRetry();
-                    CacheValue = view.Id.ToString();
-                }
+                CacheValue = _viewId;
             }
             return CacheValue;
         }
