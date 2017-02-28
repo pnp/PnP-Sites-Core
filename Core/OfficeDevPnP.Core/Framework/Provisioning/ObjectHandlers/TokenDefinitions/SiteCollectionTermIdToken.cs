5de926fec051ecc4944d7f6f9b68db844793c19b
Jakob Nøtseth <jakob@net-freak.com>
2017-03-01 00:40:42 +0100

    Two new TokenDefinitions, TermIdToken and SiteCollectionTermIdToken Added tokens to TokenParser and TokenParserTests

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/SiteCollectionTermIdToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.SharePoint.Client;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+    public class SiteCollectionTermIdToken : TokenDefinition
+    {
+        private readonly string _termId;
+
+        public SiteCollectionTermIdToken(Web ctxWeb, string termsetName, string termPath, Guid termId) : base(ctxWeb, $"{{sitecollectiontermid:{Regex.Escape(termsetName)}:{Regex.Escape(termPath)}}}")
+        {
+            _termId = termId.ToString("D");
+        }
+
+        public override string GetReplaceValue()
+        {
+            if (string.IsNullOrEmpty(CacheValue))
+            {
+                CacheValue = _termId;
+            }
+            return CacheValue;
+        }
+    }
+}
