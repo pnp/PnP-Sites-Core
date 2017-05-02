8e38d94d46d70ad91cb1418b86c54743e3706e10
Bert Jansen <bjansen@microsoft.com>
2017-05-02 10:34:24 +0200

    Serializer fix + test improvement

--- Core/OfficeDevPnP.Core.Tests/Framework/Providers/XMLProvidersTests.cs
+++ Core/OfficeDevPnP.Core.Tests/Framework/Providers/XMLProvidersTests.cs
@@ -165,7 +165,7 @@ namespace OfficeDevPnP.Core.Tests.Framework.Providers
 
             var result = provider.GetTemplates();
 
-            Assert.IsTrue(result.Count == 10);
+            Assert.IsTrue(result.Count == 11);
             Assert.IsTrue(result[0].Files.Count == 5);
             Assert.IsTrue(result[1].Files.Count == 5);
             Assert.IsTrue(result[2].Files.Count == 6);
@@ -176,6 +176,7 @@ namespace OfficeDevPnP.Core.Tests.Framework.Providers
             Assert.IsTrue(result[7].Files.Count == 1);
             Assert.IsTrue(result[8].Files.Count == 1);
             Assert.IsTrue(result[9].Files.Count == 5);
+            Assert.IsTrue(result[10].Files.Count == 2);
         }
 
         [TestMethod]
