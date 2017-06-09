1d311341c1fb67a15ca55df7b929d341f4d67cb9
Bert Jansen <bjansen@microsoft.com>
2017-06-09 13:55:30 +0200

    Quick doc change to test MSDN doc gen update

--- Core/OfficeDevPnP.Core/Entities/FieldCreationInformation.cs
+++ Core/OfficeDevPnP.Core/Entities/FieldCreationInformation.cs
@@ -4,12 +4,18 @@ using Microsoft.SharePoint.Client;
 
 namespace OfficeDevPnP.Core.Entities
 {
+    /// <summary>
+    /// Class that describes the field creation information
+    /// </summary>
     public class FieldCreationInformation
     {
         public Guid Id { get; set; }
         public string DisplayName { get; set; }
         public string InternalName { get; set; }
         public bool AddToDefaultView { get; set;}
+        /// <summary>
+        /// List of additional properties that need to be applied to the field on creation
+        /// </summary>
         public IEnumerable<KeyValuePair<string, string>> AdditionalAttributes { get; set; }
         public string FieldType { get; protected set; }
         public string Group { get; set; }
