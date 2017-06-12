f274e70b7f903efc908074663df809d4550da2a1
Ole Kristian Mørch-Storstein <okstor@gmail.com>
2017-06-12 11:53:51 +0200

    re-introducing usage of ONPREMISES compiler flag for GetWebParts as the version check has proven unreliable. This extension method is used not only by ObjectPages but by the ObjectFiles handler and in its current state it appears to make it impossible to provision web-parts to webpart pages onpremises

--- Core/OfficeDevPnP.Core/Extensions/PageExtensions.cs
+++ Core/OfficeDevPnP.Core/Extensions/PageExtensions.cs
@@ -83,7 +83,7 @@ namespace Microsoft.SharePoint.Client
 
             IEnumerable<WebPartDefinition> query;
 
-#if SP2016
+#if ONPREMISES
             // As long as we've no CSOM library that has the ZoneID we can't use the version check as things don't compile...
             query = web.Context.LoadQuery(limitedWebPartManager.WebParts.IncludeWithDefaultProperties(wp => wp.Id, wp => wp.WebPart, wp => wp.WebPart.Title, wp => wp.WebPart.Properties, wp => wp.WebPart.Hidden));
 #else
