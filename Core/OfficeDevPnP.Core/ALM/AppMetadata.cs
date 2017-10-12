e1acbbc2c40a6a2d69cb3c1ab97e380ddae2bdf8
VesaJuvonen <vesa.juvonen@hotmail.com>
2017-10-12 10:12:30 +0300

    Fixing code bug caused by me not thinking or testing what was doing.

--- Core/OfficeDevPnP.Core/ALM/AppMetadata.cs
+++ Core/OfficeDevPnP.Core/ALM/AppMetadata.cs
@@ -22,27 +22,27 @@ namespace OfficeDevPnP.Core.ALM
         /// </summary>
         [JsonProperty()]
         public bool CanUpgrade { get; internal set; }
-        [JsonProperty()]
         /// <summary>
         /// Returns whether app/solution has been deployed to the context site. 
         /// True if particular app/solution has been installed to the site.
         /// </summary>
-        public bool Deployed { get; internal set; }
         [JsonProperty()]
+        public bool Deployed { get; internal set; }
         /// <summary>
         /// Returns version of the installed app/solution in the site context. 
         /// </summary>
-        public Version InstalledVersion { get; internal set; }
         [JsonProperty()]
+        public Version InstalledVersion { get; internal set; }
         /// <summary>
         /// Returns wheter app/solution is SharePoint Framework client-side solution. 
         /// True for SPFx, False for app/add-in.
         /// </summary>
-        public bool IsClientSideSolution { get; internal set; }
         [JsonProperty()]
+        public bool IsClientSideSolution { get; internal set; }
         /// <summary>
         /// Title of the solution
         /// </summary>
+        [JsonProperty()]
         public string Title { get; internal set; }
     }
 }
