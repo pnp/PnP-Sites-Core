6a4c30a70f665b9ea544b0b3c847819189250fee
Jan Vaněk <vanekj@gmail.com>
2017-10-13 18:12:28 +0200

    When there's a reference to Default Site Groups in template and these groups do not exist, they are created.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/AssociatedGroupToken.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/AssociatedGroupToken.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Entities;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
 {
@@ -20,26 +22,56 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitio
                 this.Web.EnsureProperty(w => w.Url);
                 using (ClientContext context = this.Web.Context.Clone(this.Web.Url))
                 {
-                    context.Load(context.Web, w => w.AssociatedOwnerGroup.Title, w => w.AssociatedMemberGroup.Title, w => w.AssociatedVisitorGroup.Title);
-                    context.ExecuteQueryRetry();
-                    switch (_groupType)
+
+                    int i = 0;
+                    do
                     {
-                        case AssociatedGroupType.owners:
+
+                        context.Load(context.Web, w => w.AssociatedOwnerGroup.Title, w => w.AssociatedMemberGroup.Title, w => w.AssociatedVisitorGroup.Title, w => w.HasUniqueRoleAssignments, w => w.Title);
+                        context.ExecuteQueryRetry();
+
+                        switch (_groupType)
+                        {
+                            case AssociatedGroupType.owners:
                             {
-                                CacheValue = context.Web.AssociatedOwnerGroup.Title;
+                                if (!context.Web.AssociatedOwnerGroup.ServerObjectIsNull())
+                                    CacheValue = context.Web.AssociatedOwnerGroup.Title;
                                 break;
                             }
-                        case AssociatedGroupType.members:
+                            case AssociatedGroupType.members:
                             {
-                                CacheValue = context.Web.AssociatedMemberGroup.Title;
+                                if (!context.Web.AssociatedMemberGroup.ServerObjectIsNull())
+                                    CacheValue = context.Web.AssociatedMemberGroup.Title;
                                 break;
                             }
-                        case AssociatedGroupType.visitors:
+                            case AssociatedGroupType.visitors:
                             {
-                                CacheValue = context.Web.AssociatedVisitorGroup.Title;
+                                if (!context.Web.AssociatedVisitorGroup.ServerObjectIsNull())
+                                    CacheValue = context.Web.AssociatedVisitorGroup.Title;
                                 break;
                             }
-                    }
+                        }
+
+                        if (string.IsNullOrEmpty(CacheValue) && context.Web.HasUniqueRoleAssignments && i==0)
+                        {
+                            var web = context.Web;
+                            List<UserEntity> adminList = web.GetAdministrators();
+
+                            web.Context.Load(web.CurrentUser, u => u.LoginName);
+
+                            web.Context.ExecuteQueryRetry();
+
+                            string secondaryLoginName = web.CurrentUser.LoginName;
+
+                            web.CreateDefaultAssociatedGroups(
+                                (adminList != null && adminList.Count > 0)
+                                    ? adminList[0].LoginName
+                                    : secondaryLoginName, secondaryLoginName, web.Title);
+
+                            web.Context.ExecuteQueryRetry();
+                        }
+                        i++;
+                    } while (string.IsNullOrEmpty(CacheValue) && i<2);
                 }
             }
             return CacheValue;
