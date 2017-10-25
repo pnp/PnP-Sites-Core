8ea70af4a878bc8e299519fa6526b17699a1dc7c
Erwin van Hunen <erwin.van.hunen@outlook.com>
2017-10-25 18:06:45 +0200

    Updated template cleaner

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/NoScriptTemplateCleaner.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/NoScriptTemplateCleaner.cs
@@ -8,15 +8,29 @@ using System.Threading.Tasks;
 
 namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 {
-    public class NoScriptTemplateCleaner
+    /// <summary>
+    /// This class cleans up the template by removing artifacts that are not supported in modern sites.
+    /// </summary>
+    internal class NoScriptTemplateCleaner
     {
         public ProvisioningMessagesDelegate MessagesDelegate { get; set; }
 
         private Web _web;
+
+        /// <summary>
+        /// Createss a new instance of the template cleaner.
+        /// </summary>
+        /// <param name="web">The web to check if it is a modern site</param>
         public NoScriptTemplateCleaner(Web web)
         {
             _web = web;
         }
+
+        /// <summary>
+        /// Removes artifacts that are not supported by modern sites.
+        /// </summary>
+        /// <param name="template">The template to clean</param>
+        /// <returns></returns>
         public ProvisioningTemplate CleanUpBeforeProvisioning(ProvisioningTemplate template)
         {
             bool isNoScriptSite = _web.IsNoScriptSite();
@@ -40,32 +54,6 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
             return template;
         }
-
-        public ProvisioningTemplate CleanUpAfterExtraction(ProvisioningTemplate template)
-        {
-            bool isNoScriptSite = _web.IsNoScriptSite();
-
-            var listsToRemove = new List<ListInstance>();
-
-            foreach (var templateList in template.Lists)
-            {
-                if (isNoScriptSite && templateList.Url == "Style Library")
-                {
-                    listsToRemove.Add(templateList);
-                    WriteMessage(string.Format(CoreResources.Provisioning_ObjectHandlers_ListInstances_List__0__is_Style_Library_of_NoScript_will_Skip, templateList.Title), ProvisioningMessageType.Warning);
-                }
-            }
-            while (listsToRemove.Count > 0)
-            {
-                var listToRemove = listsToRemove[0];
-                template.Lists.Remove(listToRemove);
-                listsToRemove.RemoveAt(0);
-            }
-
-            return template;
-        }
-
-
 
         internal void WriteMessage(string message, ProvisioningMessageType messageType)
         {
