78c279c794e9b42f8c1e1ebcb9548b31ac9c1a9f
paolo@pialorsi.com <paolo@pialorsi.com>
2017-11-04 17:13:00 +0100

    Small changes to PR #1360

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectNavigation.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectNavigation.cs
@@ -137,11 +137,10 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                                     throw new ApplicationException(CoreResources.Provisioning_ObjectHandlers_Navigation_missing_global_structural_navigation);
                                 }
                                 navigationSettings.GlobalNavigation.Source = StandardNavigationSource.PortalProvider;
-                                navigationSettings.Update(TaxonomySession.GetTaxonomySession(web.Context));
-                                web.Context.ExecuteQueryRetry();
 
                                 ProvisionGlobalStructuralNavigation(web,
                                     template.Navigation.GlobalNavigation.StructuralNavigation, parser, applyingInformation.ClearNavigation, scope);
+
                                 break;
                         }
                         navigationSettings.Update(TaxonomySession.GetTaxonomySession(web.Context));
@@ -170,9 +169,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                                 {
                                     throw new ApplicationException(CoreResources.Provisioning_ObjectHandlers_Navigation_missing_current_structural_navigation);
                                 }
+                                navigationSettings.CurrentNavigation.Source = StandardNavigationSource.PortalProvider;
+
                                 ProvisionCurrentStructuralNavigation(web,
                                     template.Navigation.CurrentNavigation.StructuralNavigation, parser, applyingInformation.ClearNavigation, scope);
-                                navigationSettings.CurrentNavigation.Source = StandardNavigationSource.PortalProvider;
+
                                 break;
                             case CurrentNavigationType.Structural:
                             default:
@@ -181,9 +182,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                                 {
                                     throw new ApplicationException(CoreResources.Provisioning_ObjectHandlers_Navigation_missing_current_structural_navigation);
                                 }
+                                navigationSettings.CurrentNavigation.Source = StandardNavigationSource.PortalProvider;
+
                                 ProvisionCurrentStructuralNavigation(web,
                                     template.Navigation.CurrentNavigation.StructuralNavigation, parser, applyingInformation.ClearNavigation, scope);
-                                navigationSettings.CurrentNavigation.Source = StandardNavigationSource.PortalProvider;
+
                                 break;
                         }
                         navigationSettings.Update(TaxonomySession.GetTaxonomySession(web.Context));
