62810be6c7f47a02515d8b8e744b50f02495d664
Stéphane MAGNE <smagne@mcnext.com>
2017-11-14 16:37:41 +0100

    When exporting wiki pages webparts, create a token for web url, and not only web serverRelativeUrl. Used for exemple in excel steelsheet webpart, to store link to excel. It wasn't replaced and kept source web url

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectPageContents.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectPageContents.cs
@@ -271,6 +271,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             xml = Regex.Replace(xml, "(\"" + web.ServerRelativeUrl + ")(?!&)", "\"{site}", RegexOptions.IgnoreCase);
             xml = Regex.Replace(xml, "'" + web.ServerRelativeUrl, "'{site}", RegexOptions.IgnoreCase);
             xml = Regex.Replace(xml, ">" + web.ServerRelativeUrl, ">{site}", RegexOptions.IgnoreCase);
+            xml = Regex.Replace(xml, ">" + web.Url, ">{site}", RegexOptions.IgnoreCase);
             return xml;
         }
 
