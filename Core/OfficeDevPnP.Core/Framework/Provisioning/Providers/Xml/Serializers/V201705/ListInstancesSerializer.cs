5afadaeece91ae5959a60c279233d326562a8774
paolo@pialorsi.com <paolo@pialorsi.com>
2017-12-05 16:48:11 +0100

    Fix to ReadSecuritySpecified handling for ListInstances

--- Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/Serializers/V201705/ListInstancesSerializer.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/Serializers/V201705/ListInstancesSerializer.cs
@@ -168,7 +168,12 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml.Serializers.V20
                 resolvers.Add($"{listInstanceType}.DraftVersionVisibilitySpecified", new ExpressionValueResolver(() => true));
                 resolvers.Add($"{listInstanceType}.MaxVersionLimitSpecified", new ExpressionValueResolver(() => true));
                 resolvers.Add($"{listInstanceType}.MinorVersionLimitSpecified", new ExpressionValueResolver(() => true));
-                resolvers.Add($"{listInstanceType}.ReadSecuritySpecified", new ExpressionValueResolver(() => true));
+                resolvers.Add($"{listInstanceType}.ReadSecuritySpecified", new ExpressionValueResolver((s, v) =>
+                {
+                    var value = (Int32)s.GetPublicInstancePropertyValue("ReadSecurity");
+                    return (value == 1 || value == 2);
+                }
+                ));
 
                 // IRM Settings
                 resolvers.Add($"{listInstanceType}.IRMSettings", new IRMSettingsFromModelToSchemaTypeResolver());
