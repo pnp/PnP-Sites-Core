3fc59eec3fa9815f9384896b135f2fcb307fb5e5
Ole Kristian Mørch-Storstein <okstor@gmail.com>
2017-12-13 11:06:20 +0100

    adding ToListId attribute as another attribute that can be replaced in the exported xaml

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectWorkflows.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectWorkflows.cs
@@ -395,14 +395,22 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
         {
             // Tokenize XAML to replace any ListId attribute with the corresponding token
             XElement xamlDocument = XElement.Parse(xaml);
-            var elements = (IEnumerable)xamlDocument.XPathEvaluate("//child::*[@ListId]");
+            string[] listIdAttributes = {"ListId", "ToListId"};
+            
+            var elements = (IEnumerable)xamlDocument.XPathEvaluate($"//child::*[@{listIdAttributes[0]}|@{listIdAttributes[1]}]");
 
-            if (elements != null)
+            if (elements != null) // always true, consider removing 
             {
                 foreach (var element in elements.Cast<XElement>())
                 {
-                    var listId = element.Attribute("ListId").Value;
-                    element.SetAttributeValue("ListId", TokenizeListIdProperty(listId, lists));
+                    foreach (var listIdAttribute in listIdAttributes)
+                    {
+                        if (element.Attribute(listIdAttribute) != null)
+                        {
+                            var listId = element.Attribute(listIdAttribute).Value;
+                            element.SetAttributeValue(listIdAttribute, TokenizeListIdProperty(listId, lists));
+                        }
+                    }
                 }
 
                 xaml = xamlDocument.ToString();
