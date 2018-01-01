8411d0daade4aabd0f98653777464beb58bf6b30
Yannick Plenevaux <yannick.plenevaux@gmail.com>
2018-01-01 22:56:52 +0100

    allows to specify a custom cache provider for Web API SharePoint context handling

---
+++ Core/OfficeDevPnP.Core/Utilities/Cache/SimpleAspNetCacheProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OfficeDevPnP.Core.Utilities.Cache
+{
+    /// <summary>
+    /// A Cache Provider implementation that uses the HttpRuntime Cache class as underlying implementation
+    /// Is a suitable choice for implementation of standard IIS Distributed cache technique
+    /// </summary>
+    public class SimpleAspNetCacheProvider : ICacheProvider
+    {
+        public T Get<T>(string cacheKey)
+        {
+            return (T)HttpRuntime.Cache.Get(cacheKey);
+        }
+
+        public void Put<T>(string cacheKey, T item)
+        {
+            HttpRuntime.Cache.Insert(cacheKey, item);
+        }
+    }
+}
