8717070bd63ea9d900da3a4eea4e19aebafda4d4
Bert Jansen <bjansen@microsoft.com>
2018-01-17 12:59:49 +0100

    Improved client side page extraction by - Supporting easy calling of page extraction from an extensibilty provider - Supporting the detection and export of assets referenced by client side web parts - 7 new tokens to support full tokenization of client side web part json data - Site image can be returned as fully qualified url by SPO: enable extraction in that case - Make use of the specified Folder value when uploading files

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/FileUniqueIdToken.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Text.RegularExpressions;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+    internal class FileUniqueIdToken : TokenDefinition
+    {
+        private readonly string _value = null;
+        public FileUniqueIdToken(Web web, string siteRelativePath, Guid uniqueId)
+            : base(web, $"{{fileuniqueid:{Regex.Escape(siteRelativePath)}}}")
+        {
+            _value = uniqueId.ToString();
+        }
+
+        public override string GetReplaceValue()
+        {
+            if (string.IsNullOrEmpty(CacheValue))
+            {
+                CacheValue = _value;
+            }
+            return CacheValue;
+        }
+    }
+}
+
