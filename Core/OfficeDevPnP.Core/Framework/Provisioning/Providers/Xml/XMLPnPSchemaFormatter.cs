5c2cbb4cf7b71319a602c3fe8f79cbdc9ee6aa80
paolo@pialorsi.com <paolo@pialorsi.com>
2018-01-17 18:42:16 +0100

    Switch from schema 201705 to schema 201801 as the default one.

--- Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/XMLPnPSchemaFormatter.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/XMLPnPSchemaFormatter.cs
@@ -29,8 +29,8 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml
         {
             get
             {
-                return (new XMLPnPSchemaV201705Serializer());
-                // return (new XMLPnPSchemaV201801Serializer());
+                // return (new XMLPnPSchemaV201705Serializer());
+                return (new XMLPnPSchemaV201801Serializer());
             }
         }
 
@@ -55,11 +55,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml
                     return (new XMLPnPSchemaV201512Formatter());
                 case XMLPnPSchemaVersion.V201605:
                     return (new XMLPnPSchemaV201605Formatter());
-                case XMLPnPSchemaVersion.V201801:
-                    return (new XMLPnPSchemaV201801Serializer());
                 case XMLPnPSchemaVersion.V201705:
-                default:
                     return (new XMLPnPSchemaV201705Serializer());
+                case XMLPnPSchemaVersion.V201801:
+                default:
+                    return (new XMLPnPSchemaV201801Serializer());
             }
         }
 
@@ -84,11 +84,11 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml
 #pragma warning restore CS0618 // Type or member is obsolete
                 case XMLConstants.PROVISIONING_SCHEMA_NAMESPACE_2016_05:
                     return (new XMLPnPSchemaV201605Formatter());
-                case XMLConstants.PROVISIONING_SCHEMA_NAMESPACE_2018_01:
-                    return (new XMLPnPSchemaV201801Serializer());
                 case XMLConstants.PROVISIONING_SCHEMA_NAMESPACE_2017_05:
-                default:
                     return (new XMLPnPSchemaV201705Serializer());
+                case XMLConstants.PROVISIONING_SCHEMA_NAMESPACE_2018_01:
+                default:
+                    return (new XMLPnPSchemaV201801Serializer());
             }
         }
 
