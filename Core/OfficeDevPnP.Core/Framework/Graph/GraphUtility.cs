e87dc108238a14b4b49626959b7c6eebf48db21b
Vipul Kelkar <vipul_kelkar@yahoo.com>
2018-02-07 13:52:32 +0530

    Updated the Microsoft.Graph nuget package to the latest version. Added GraphUtility class to perform operations using Graph api. Added a method to send Guest user invitation. This method can further be used in PnP Powershell to write command to send user invite.

---
+++ Core/OfficeDevPnP.Core/Framework/Graph/GraphUtility.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Graph;
+using OfficeDevPnP.Core.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Graph
+{
+    public static class GraphUtility
+    {
+        private const int defaultRetryCount = 10;
+        private const int defaultDelay = 500;
+
+        /// <summary>
+        ///  Creates a new GraphServiceClient instance using a custom PnPHttpProvider
+        /// </summary>
+        /// <param name="accessToken">The OAuth 2.0 Access Token to configure the HTTP bearer Authorization Header</param>
+        /// <param name="retryCount">Number of times to retry the request in case of throttling</param>
+        /// <param name="delay">Milliseconds to wait before retrying the request.</param>
+        /// <returns></returns>
+        public static GraphServiceClient CreateGraphClient(String accessToken, int retryCount = defaultRetryCount, int delay = defaultDelay)
+        {
+            // Creates a new GraphServiceClient instance using a custom PnPHttpProvider
+            // which natively supports retry logic for throttled requests
+            // Default are 10 retries with a base delay of 500ms
+            var result = new GraphServiceClient(new DelegateAuthenticationProvider(
+                        async (requestMessage) =>
+                        {
+                            if (!String.IsNullOrEmpty(accessToken))
+                            {
+                                // Configure the HTTP bearer Authorization Header
+                                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+                            }
+                        }), new PnPHttpProvider(retryCount, delay));
+
+            return (result);
+        }
+
+        public static Invitation InviteGuestUser(string accessToken, string guestUserEmail, string redirectUri, string customizedMessage = "", string guestUserDisplayName = "")
+        {
+            Invitation inviteUserResponse = null;
+
+            try
+            {
+                Invitation invite = new Invitation();
+                invite.InvitedUserEmailAddress = guestUserEmail;
+                if (!string.IsNullOrWhiteSpace(guestUserDisplayName))
+                {
+                    invite.InvitedUserDisplayName = guestUserDisplayName;
+                }
+                invite.InviteRedirectUrl = redirectUri;
+                invite.SendInvitationMessage = true;
+
+                // Form the invite email message body
+                if (!string.IsNullOrWhiteSpace(customizedMessage))
+                {
+                    InvitedUserMessageInfo inviteMsgInfo = new InvitedUserMessageInfo();
+                    inviteMsgInfo.CustomizedMessageBody = customizedMessage;
+                    invite.InvitedUserMessageInfo = inviteMsgInfo;
+                }
+
+                // Create the graph client and send the invitation.
+                GraphServiceClient graphClient = CreateGraphClient(accessToken);
+                inviteUserResponse = graphClient.Invitations.Request().AddAsync(invite).Result;
+            }
+            catch (ServiceException ex)
+            {
+                Log.Error(Constants.LOGGING_SOURCE, CoreResources.GraphExtensions_ErrorOccured, ex.Error.Message);
+                throw;
+            }
+            return inviteUserResponse;
+        }
+    }
+}
