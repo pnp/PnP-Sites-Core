3da68e5a668f03a911e4d92144a929d6800ebfb1
Steve Beaugé <stevegit@outlook.com>
2018-05-28 23:50:36 +0200

    Changed lookup field privisioning to also handle list url in `List` attribute

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/Utilities/FieldUtilities.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Xml.Linq;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.Utilities
+{
+    public static class FieldUtilities
+    {
+        public static string FixLookupField(string fieldXml, Web web)
+        {
+            var fieldElement = XElement.Parse(fieldXml);
+            if ((string)fieldElement.Attribute("Type") == "Lookup")
+            {
+                var listAttr = (string)fieldElement.Attribute("List");
+                if (!Guid.TryParse(listAttr, out Guid _))
+                {
+                    var targetList = web.GetList($"{web.ServerRelativeUrl.TrimEnd('/')}/{listAttr}");
+                    fieldElement.SetAttributeValue("List", targetList.EnsureProperty(l => l.Id).ToString("B"));
+                    return fieldElement.ToString();
+                }
+            }
+
+            return fieldXml;
+        }
+    }
+}
