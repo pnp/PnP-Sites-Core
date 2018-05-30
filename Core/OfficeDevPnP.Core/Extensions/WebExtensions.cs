c456dc878ab20eb4656f557967be01a18b297196
Massimo Prota <massimo.prota@rapidcircle.com>
2018-05-30 20:48:46 +0200

    Enabling RequestAccess Web extensions methods for OnPrem , as they're supported by CSOM (15.0, 16.0)

--- Core/OfficeDevPnP.Core/Extensions/WebExtensions.cs
+++ Core/OfficeDevPnP.Core/Extensions/WebExtensions.cs
@@ -1229,6 +1229,7 @@ namespace Microsoft.SharePoint.Client
         #endregion
 
         #region Request Access
+#if !ONPREMISES
         /// <summary>
         /// Disables the request access on the web.
         /// </summary>
@@ -1308,6 +1309,7 @@ namespace Microsoft.SharePoint.Client
 
             return emails;
         }
+#endif
         #endregion
 
         /// <summary>
