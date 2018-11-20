cc5f7791153f7d71993d05b9892180871b3081f7
Erwin van Hunen <erwin.van.hunen@outlook.com>
2018-11-20 17:27:45 +0100

    added everyonebutexternalusers token

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/TokenDefinitions/EveryoneButExternalUsersToken.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SharePoint.Client;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers.TokenDefinitions
+{
+    internal class EveryoneButExternalUsersToken : TokenDefinition
+    {
+        public EveryoneButExternalUsersToken(Web web)
+            : base(web, $"{{everyonebutexternalusers}}")
+        {
+
+        }
+
+        public override string GetReplaceValue()
+        {
+           return $"c:0-.f|rolemanager|spo-grid-all-users/{Web.GetAuthenticationRealm()}";
+        }
+    }
+}
