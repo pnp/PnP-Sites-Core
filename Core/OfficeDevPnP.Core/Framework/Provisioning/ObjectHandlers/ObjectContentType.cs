fa1b2e4dd8a795e4c950995d594b7433405f8ae7
Bert Jansen <bjansen@microsoft.com>
2018-12-07 09:48:37 +0100

    Move EnsureProperties higher up as otherwise in can trigger executequery before the needed update statement. In this case this broke the fieldref ordering

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectContentType.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectContentType.cs
@@ -334,6 +334,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             var group = parser.ParseString(templateContentType.Group);
 
             var createdCT = web.CreateContentType(name, description, id, group);
+            createdCT.EnsureProperties(ct => ct.ReadOnly, ct => ct.Hidden, ct => ct.Sealed);
 
             List<FieldRef> fieldsRefsToProcess = new List<FieldRef>();
             foreach (FieldRef fr in templateContentType.FieldRefs)
@@ -384,8 +385,6 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             //will be added at the end. To fix this issue we ordering the fields once more.
 
             createdCT.FieldLinks.Reorder(templateContentType.FieldRefs.Select(fld => parser.ParseString(fld.Name)).ToArray());
-            createdCT.EnsureProperties(ct => ct.ReadOnly, ct => ct.Hidden, ct => ct.Sealed);
-
             if (createdCT.ReadOnly != templateContentType.ReadOnly)
             {
                 createdCT.ReadOnly = templateContentType.ReadOnly;
