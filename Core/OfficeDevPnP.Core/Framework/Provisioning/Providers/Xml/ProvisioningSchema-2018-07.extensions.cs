0d593d38b5800121c749726af92b6d815eb2e65d
christian zuellig <christian.zuellig@mondaycoffee.com>
2019-03-16 13:20:56 +0100

    Add Schema extension for ZoneEmphasis in CanvasSection

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/ProvisioningSchema-2018-07.extensions.cs
@@ -0,0 +1,40 @@
+﻿namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml.V201807
+{
+
+    public partial class CanvasSection
+    {
+        private int zoneEmphasisField;
+
+        private bool zoneEmphasisFieldSpecified;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        [System.ComponentModel.DefaultValueAttribute(0)]
+        public int ZoneEmphasis
+        {
+            get
+            {
+                return this.zoneEmphasisField;
+            }
+            set
+            {
+                this.zoneEmphasisField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool ZoneEmphasisFieldSpecified
+        {
+            get
+            {
+                return this.zoneEmphasisFieldSpecified;
+            }
+            set
+            {
+                this.zoneEmphasisFieldSpecified = value;
+            }
+        }
+    }
+
+}
