8eaf525df254e5c33c2a78a1a5782aeaef150864
paolo@pialorsi.com <paolo@pialorsi.com>
2019-04-02 16:49:44 +0200

    Added AAD Users to Domain Model

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Model/Teams/ProvisioningTeams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Model.Teams
+{
+    /// <summary>
+    /// Defines a complex type declaring settings for provisioning Microsoft Teams objects
+    /// </summary>
+    public class ProvisioningTeams
+    {
+    }
+}
