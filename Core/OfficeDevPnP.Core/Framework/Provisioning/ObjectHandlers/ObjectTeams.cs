066cd75c2044023bcb6815713747805128d461b7
paolo@pialorsi.com <paolo@pialorsi.com>
2019-04-11 17:53:30 +0200

    Added basic support for Teams Object Handler

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectTeams.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Diagnostics;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
+{
+    /// <summary>
+    /// Object Handler to manage Microsoft Teams stuff
+    /// </summary>
+    internal class ObjectTeams : ObjectHandlerBase
+    {
+        public override string Name
+        {
+            get { return "Teams"; }
+        }
+
+        public override TokenParser ProvisionObjects(Web web, ProvisioningTemplate template, TokenParser parser, ProvisioningTemplateApplyingInformation applyingInformation)
+        {
+            using (var scope = new PnPMonitoredScope(this.Name))
+            {
+                // Here we're going to invoke the handlers for:
+                // - Teams Templates
+                // - Teams
+                // - Apps
+            }
+
+            return (parser);
+        }
+
+        public override ProvisioningTemplate ExtractObjects(Web web, ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInfo)
+        {
+            // So far, no extraction
+            return template;
+        }
+
+        public override bool WillProvision(Web web, ProvisioningTemplate template, ProvisioningTemplateApplyingInformation applyingInformation)
+        {
+            if (!_willProvision.HasValue)
+            {
+                _willProvision = template.ParentHierarchy.Teams?.TeamTemplates?.Any() |
+                    template.ParentHierarchy.Teams?.Teams?.Any();
+            }
+            return _willProvision.Value;
+        }
+
+        public override bool WillExtract(Web web, ProvisioningTemplate template, ProvisioningTemplateCreationInformation creationInfo)
+        {
+            if (!_willExtract.HasValue)
+            {
+                _willExtract = false;
+            }
+            return _willExtract.Value;
+        }
+    }
+}
