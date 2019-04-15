2d418518b4bf82d589051e7dcb1080f4a430c40f
paolo@pialorsi.com <paolo@pialorsi.com>
2019-04-15 17:15:01 +0200

    Initial CanProvision handling implementation

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/CanProvisionHandlers/CanProvisionHandlerBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.CanProvisionHandlers
+{
+    /// <summary>
+    /// Base class to test if the Provisioning Template can be provisioned on the target environment
+    /// </summary>
+    internal abstract class CanProvisionHandlerBase
+    {
+    }
+}
