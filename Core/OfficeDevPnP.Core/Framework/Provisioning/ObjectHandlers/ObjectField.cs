03589de4859e81b2121d03e3ab03f4fc2bb38af3
Koen Zomers <koen@zomers.eu>
2019-07-18 16:20:52 +0200

    When providing a FieldDefaults under a ListInstance for a taxonomy field and setting the ID to -1, this code change will make sure to update the -1 to the actual ID as it exists in the hidden TaxonomyHiddenList list or if it doesn't exist in there yet, create a new entry for it and update the -1 to the ID of this new entry

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectField.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectField.cs
@@ -407,7 +407,7 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
             }
         }
 
-        private static string GetTaxonomyFieldValidatedValue(TaxonomyField field, string defaultValue)
+        public static string GetTaxonomyFieldValidatedValue(TaxonomyField field, string defaultValue)
         {
             string res = null;
             object parsedValue = null;
