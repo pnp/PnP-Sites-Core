fc4d47589b6f5f36cdaf01d9e42602e6bcb4773a
paolo@pialorsi.com <paolo@pialorsi.com>
2019-10-05 18:25:52 +0200

    Added Client Side Pages translations at schema and serialization level

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Model/SharePoint/ModernExperiences/TranslatedClientSidePage.cs
@@ -0,0 +1,68 @@
+﻿using OfficeDevPnP.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Model
+{
+    /// <summary>
+    /// Defines a Translated ClientSidePage
+    /// </summary>
+    public partial class TranslatedClientSidePage : BaseClientSidePage
+    {
+        #region Public Members
+
+        /// <summary>
+        /// Defines the Locale ID of a Localization Language
+        /// </summary>
+        public String LCID { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor for TranslatedClientSidePage class
+        /// </summary>
+        public TranslatedClientSidePage()
+        {
+        }
+
+        #endregion
+
+        #region Comparison code
+
+        /// <summary>
+        /// Gets the hash code
+        /// </summary>
+        /// <returns>Returns HashCode</returns>
+        protected override int GetInheritedHashCode()
+        {
+            return (String.Format("{0}|",
+                this.LCID.GetHashCode()
+            ).GetHashCode());
+        }
+
+        /// <summary>
+        /// Compares TranslatedClientSidePage object based on LCID
+        /// </summary>
+        /// <param name="other">TranslatedClientSidePage Class object</param>
+        /// <returns>true if the TranslatedClientSidePage object is equal to the current object; otherwise, false.</returns>
+        protected override bool EqualsInherited(BaseClientSidePage other)
+        {
+            var otherTyped = other as TranslatedClientSidePage;
+
+            if (otherTyped == null)
+            {
+                return (false);
+            }
+
+            return (this.LCID == otherTyped.LCID
+                );
+        }
+
+        #endregion
+    }
+}
---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/Resolvers/V201909/ClientSidePageHeaderFromSchemaToModelTypeResolver.cs
@@ -0,0 +1,34 @@
+﻿using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OfficeDevPnP.Core.Extensions;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml.Resolvers.V201909
+{
+    internal class ClientSidePageHeaderFromSchemaToModelTypeResolver : ITypeResolver
+    {
+        public string Name => this.GetType().Name;
+
+        public bool CustomCollectionResolver => false;
+
+        public ClientSidePageHeaderFromSchemaToModelTypeResolver()
+        {
+        }
+
+        public object Resolve(object source, Dictionary<String, IResolver> resolvers = null, Boolean recursive = false)
+        {
+            var result = new ClientSidePageHeader();
+            var header = source.GetPublicInstancePropertyValue("Header");
+
+            if (null != header)
+            {
+                PnPObjectsMapper.MapProperties(header, result, resolvers, recursive);
+            }
+
+            return (result);
+        }
+    }
+}
