ac7f6907e115a9a5853403d50b2e7d5c1c5abc45
paolo@pialorsi.com <paolo@pialorsi.com>
2019-10-07 12:11:00 +0200

    Fixed bug with Security element in ClientSidePage

---
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/Providers/Xml/Resolvers/V201807/ClientSidePageSecurityFromModelToSchemaTypeResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml.Resolvers.V201807
+{
+    internal class ClientSidePageSecurityFromModelToSchemaTypeResolver : ITypeResolver
+    {
+        public string Name => this.GetType().Name;
+
+        public bool CustomCollectionResolver => false;
+
+
+        public ClientSidePageSecurityFromModelToSchemaTypeResolver()
+        {
+        }
+
+        public object Resolve(object source, Dictionary<String, IResolver> resolvers = null, Boolean recursive = false)
+        {
+            Object result = null;
+
+            // Try with the tenant-wide AppCatalog
+            var page = source as Model.ClientSidePage;
+            var security = page?.Security;
+
+            // If we have security settings
+            if (null != security && 
+                security.RoleAssignments != null &&
+                security.RoleAssignments.Count > 0)
+            {
+                // Map them to the output
+                var securityTypeName = $"{PnPSerializationScope.Current?.BaseSchemaNamespace}.ObjectSecurity, {PnPSerializationScope.Current?.BaseSchemaAssemblyName}";
+                var securityType = Type.GetType(securityTypeName, true);
+                result = Activator.CreateInstance(securityType);
+
+                PnPObjectsMapper.MapProperties(security, result, resolvers, recursive);
+            }
+
+            return (result);
+        }
+    }
+}
