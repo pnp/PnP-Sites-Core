492de670fe06aade057ec250f2e815c3ac436dc4
Erwin van Hunen <erwin.van.hunen@outlook.com>
2019-10-24 12:51:33 +0200

    expanded string normalization

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectTeams.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectTeams.cs
@@ -1060,25 +1060,33 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
         private static string ReplaceAccentedCharactersWithLatin(string str)
         {
-            const string a = "[äåàáâã]";
+            const string a = "[äåàáâãæ]";
             var regex = new Regex(a, RegexOptions.IgnoreCase);
             str = regex.Replace(str, "a");
 
-            const string e = "[èéêë]";
+            const string e = "[èéêëēĕėęě]";
             regex = new Regex(e, RegexOptions.IgnoreCase);
             str = regex.Replace(str, "e");
 
-            const string i = "[ìíîï]";
+            const string i = "[ìíîïĩīĭįı]";
             regex = new Regex(i, RegexOptions.IgnoreCase);
             str = regex.Replace(str, "i");
 
-            const string o = "[öòóôõ]";
+            const string o = "[öòóôõø]";
             regex = new Regex(o, RegexOptions.IgnoreCase);
             str = regex.Replace(str, "o");
 
             const string u = "[üùúû]";
             regex = new Regex(u, RegexOptions.IgnoreCase);
             str = regex.Replace(str, "u");
+
+            const string c = "[çċčćĉ]";
+            regex = new Regex(c, RegexOptions.IgnoreCase);
+            str = regex.Replace(str, "c");
+
+            const string d = "[ðďđđ]";
+            regex = new Regex(d, RegexOptions.IgnoreCase);
+            str = regex.Replace(str, "d");
 
             return str;
         }
