cda011eb677741b5127be73cccdef239f2e97d87
Jens Otto Hatlevold <jensotto@outlook.com>
2020-03-05 15:52:22 +0100

    Added RebuildListTokens before and after processing views

--- Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
+++ Core/OfficeDevPnP.Core/Framework/Provisioning/ObjectHandlers/ObjectListInstance.cs
@@ -157,6 +157,8 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
                     // We stop here unless we reached the last provisioning stop of the list
                     if (step == FieldAndListProvisioningStepHelper.Step.ListSettings)
                     {
+                        parser.RebuildListTokens(web);
+
                         #region Default Field Values
 
                         foreach (var listInfo in processedLists)
@@ -170,9 +172,10 @@ namespace OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers
 
                         foreach (var listInfo in processedLists)
                         {
-                            parser.RebuildListTokens(web);
                             ProcessViews(web, parser, scope, listInfo);
                         }
+
+                        parser.RebuildListTokens(web);
 
                         #endregion Views
 
