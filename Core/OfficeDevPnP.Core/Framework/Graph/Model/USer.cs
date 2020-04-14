5a0d0c7104769f3d39b57c9cdaa3331ca220566e
Koen Zomers <koen@zomers.eu>
2020-04-14 21:52:21 +0200

    Adding support for retrieval of Users from Azure Active Directory through the Microsoft Graph API

---
+++ Core/OfficeDevPnP.Core/Framework/Graph/Model/USer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OfficeDevPnP.Core.Framework.Graph.Model
+{
+    /// <summary>
+    /// Defines a Microsoft Graph user
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Business phone numbers for the user
+        /// </summary>
+        public List<string> BusinessPhones { get; set; }
+
+        /// <summary>
+        /// Display name for the user
+        /// </summary>
+        public string DisplayName { get; set; }
+        
+        /// <summary>
+        /// Given name of the user
+        /// </summary>
+        public string GivenName { get; set; }
+
+        /// <summary>
+        /// Job title of the user
+        /// </summary>
+        public string JobTitle { get; set; }
+
+        /// <summary>
+        /// Primary e-mail address of the user
+        /// </summary>
+        public string Mail { get; set; }
+
+        /// <summary>
+        /// Mobile phone number of the user
+        /// </summary>
+        public string MobilePhone { get; set; }
+        
+        /// <summary>
+        /// Office location of the user
+        /// </summary>
+        public string OfficeLocation { get; set; }
+
+        /// <summary>
+        /// Preferred language of the user
+        /// </summary>
+        public string PreferredLanguage { get; set; }
+
+        /// <summary>
+        /// Surname of the user
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// User Principal Name (UPN) of the user
+        /// </summary>
+        public string UserPrincipalName { get; set; }
+
+        /// <summary>
+        /// Unique identifier of the user
+        /// </summary>
+        public Guid? Id { get; set; }
+    }
+}
