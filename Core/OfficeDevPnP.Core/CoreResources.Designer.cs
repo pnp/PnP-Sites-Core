3a2bbbec9a36df157b925bc98d55d60a9e103e2d
gszdev <stefan.zaeh@gabo.de>
2020-06-25 15:56:24 +0200

    Updated SP-Responsive-UI files (js, css) Using min files for Release builds SP-Responsive-UI files source: https://github.com/pnp/PnP-Tools/tree/master/Solutions/SharePoint.UI.Responsive

--- Core/OfficeDevPnP.Core/CoreResources.Designer.cs
+++ Core/OfficeDevPnP.Core/CoreResources.Designer.cs
@@ -2599,7 +2599,7 @@ namespace OfficeDevPnP.Core {
         /// */
         ///
         ////*
-        /// * PnPResponsiveApp
+        /// * PnPResponsiveApp Namespace
         /// * @namespace
         /// */
         ///if (window.hasOwnProperty(&apos;Type&apos;)) {
@@ -2609,10 +2609,10 @@ namespace OfficeDevPnP.Core {
         ///}
         ///
         ////**
-        /// * PnP Responsive
+        /// * PnP Responsive Main Class
         /// * @class
         /// */
-        ///PnPResponsiveApp.Main = (function ()  [rest of string was truncated]&quot;;.
+        ///PnPResponsiveApp [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string SP_Responsive_UI {
             get {
@@ -2621,38 +2621,57 @@ namespace OfficeDevPnP.Core {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .mobile-only {
+        ///   Looks up a localized string similar to /*
+        /// * Hide all HTML nodes that don&apos;t have to appear
+        /// */
+        ///
+        ///.mobile-only {
         ///    display: none;
         ///}
         ///
-        ///#sideNavBox, #contentRow, #siteIcon {
+        ///#sideNavBox,
+        ///#contentRow,
+        ///#siteIcon {
         ///    -webkit-transition: all 400ms ease;
         ///    -moz-transition: all 400ms ease;
         ///    -ms-transition: all 400ms ease;
         ///    -o-transition: all 400ms ease;
         ///    transition: all 400ms ease;
         ///}
-        ///
+        ///#s4-bodyContainer .contentwrapper,
         ///#contentBox {
         ///    min-width: auto;
         ///}
         ///
         ////* Make sure dialog windows don&apos;t break */
+        ///
         ///.ms-dialog #contentRow {
         ///    margin-left: 0;
         ///}
         ///
-        ///.ms-dialog #contentBox {
-        ///    margin-top: 0;
-        ///}
-        ///
-        ///.ms-dialog #s4-bodyContainer {
-        ///    min-width: 0;
-        ///   [rest of string was truncated]&quot;;.
+        ///.ms-d [rest of string was truncated]&quot;;.
         /// </summary>
         internal static string SP_Responsive_UI_CSS {
             get {
                 return ResourceManager.GetString("SP_Responsive_UI_CSS", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to .mobile-only{display:none}#contentRow,#sideNavBox,#siteIcon{-webkit-transition:all .4s ease;-moz-transition:all .4s ease;-ms-transition:all .4s ease;-o-transition:all .4s ease;transition:all .4s ease}#contentBox,#s4-bodyContainer .contentwrapper{min-width:auto}.ms-dialog #contentRow{margin-left:0}.ms-dialog #contentBox{margin-top:0}.ms-dialog #s4-bodyContainer{min-width:0;font-size:1em}#s4-bodyContainer .contentwrapper{min-width:0;margin-top:0}#suiteBar #suiteBarLeft #PnP_MainLink_Hamburger{min-width:30px;p [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SP_Responsive_UI_CSS_min {
+            get {
+                return ResourceManager.GetString("SP_Responsive_UI_CSS_min", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to function responsiveStartup(){var e=PnPResponsiveApp.Main.getInstance();e.addViewport(),e.init()}window.hasOwnProperty(&quot;Type&quot;)?Type.registerNamespace(&quot;PnPResponsiveApp&quot;):window.PnPResponsiveApp=window.PnPResponsiveApp||{},PnPResponsiveApp.Main=function(){function e(e,n){if(t(e,n)){var i=new RegExp(&quot;(\\s|^)&quot;+n+&quot;(\\s|$)&quot;);e.className=e.className.replace(i,&quot; &quot;)}else e.className=e.className+&quot; &quot;+n}function t(e,t){try{return-1!==(&quot; &quot;+e.className+&quot; &quot;).indexOf(&quot; &quot;+t+&quot; &quot;)}catch(e){return!1}}function n(e){var t=docume [rest of string was truncated]&quot;;.
+        /// </summary>
+        internal static string SP_Responsive_UI_min {
+            get {
+                return ResourceManager.GetString("SP_Responsive_UI_min", resourceCulture);
             }
         }
         
